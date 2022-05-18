b055cd5be195bd9f92d72feb8a838975f1dc7815
sharma.59 <sharma.59@iitjac.in>
2022-05-18 17:01:21 +0530

    final semester commit

--- cgpa_calculator.py
+++ cgpa_calculator.py
@@ -42,7 +42,7 @@ grades = [
     (3,8),
     (1.5,10),
     (4.5,8), # cn
-    (4.5,9), #dl 
+    (4.5,10), #dl 
 ]
 
 wt_sum = 0
@@ -53,3 +53,22 @@ for (credit, grade) in grades:
     wt_sum += (credit * grade)
 
 print(f"cgpa: {wt_sum/total_credit:.2f}")
+
+sgpa = [
+    (3,10),
+    (4,8),
+    (3,8),
+    (1.5,10),
+    (4.5,8), # cn
+    (4.5,10), #dl 
+]
+
+
+wt_credit = 0
+total_credit = 0
+for credit,grade in sgpa:
+    wt_credit += (credit * grade)
+    total_credit += credit
+
+print(f'sgpa {wt_credit/total_credit:.2f}')
+
